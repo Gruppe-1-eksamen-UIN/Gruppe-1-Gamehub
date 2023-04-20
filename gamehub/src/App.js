db66c7f418f6cb6047b237ea6256217db0842b8e
Muhsin310302 <Muhsin310302@hotmail.com>
2023-04-20 19:53:38 +0200

    lagt til uferdig api

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [game, setGame] = useState([]);
+
+  const find = async() =>{
+    const play = await fetch('https://api.rawg.io/api/games?key=5d8741db23a14d7f88a2c6ccd843ee6c')
+    console.log(game)
+    setGame(game.data.search)
+
+  }
+
+  useEffect(() =>{
+    find()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ul>
+
+   </ul>
   );
 }
 
