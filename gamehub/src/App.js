905d0039cfd9a4e8bad706dfd97ee99e6b0cd904
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-04-21 18:36:08 +0200

    satt inn bootstrap

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -4,11 +4,8 @@ import './css/main.css'
 
 function App() {
   return (
-    <div className="game-box">
-      <header className="Game"> Game #1</header>
-      <image className="Game-image" href=""></image>
-      <h2 className="genre">Genre</h2>
-      <p>Hours played: 199</p>
+    <div className="">
+    
     </div>
   );
 }
