abdad86208fbdf8aec94bb473bbe2d5e8841294c
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-04-21 18:06:24 +0200

    testing av sass

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -4,8 +4,11 @@ import './css/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <p>gamehub app</p>
+    <div className="game-box">
+      <header className="Game"> Game #1</header>
+      <image className="Game-image" href=""></image>
+      <h2 className="genre">Genre</h2>
+      <p>Hours played: 199</p>
     </div>
   );
 }
