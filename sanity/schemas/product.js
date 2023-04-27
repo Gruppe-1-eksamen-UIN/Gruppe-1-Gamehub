1142a5a9a638ac133946926d7a0cc10ef502a90a
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2023-04-27 16:00:25 +0200

    tester sanity setup

---
+++ sanity/schemas/product.js
@@ -0,0 +1,26 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'genre',
+        title: 'Genre',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+    ],
+  };
+  
