26c9acd33419a299ca6aff15f48cb7948f7d4281
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-05-03 21:02:06 +0200

    små endringer til dataset

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import './css/main.css'
 
+
 function App() {
-  return (
-    <div className="">
-    
+    <div className="">  
+     
     </div>
-  );
 }
 
 export default App;
