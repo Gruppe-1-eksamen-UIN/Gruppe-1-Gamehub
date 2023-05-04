526d4926a2790810df63d5c1f42e5a4650794ff0
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-05-04 18:20:01 +0200

    hentet fra sanity 2 spill.
    Må fikses på å hente image url

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import './css/main.css'
+import client from './utils/sanity/client';
 
+import {useState, useEffect} from "react"
 
 function App() {
-    <div className="">  
-     
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const result = await client.fetch('*[_type == "product"]{name, genre, image}');
+      setProducts(result);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            <img src={product.image.asset.url + '?w=200&h=200&fit=crop'} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.genre}</p>
+            {console.log(product.image.asset.url)}
+          </li>
+        ))}
+      </ul>
     </div>
+  );
 }
 
 export default App;
+
--- gamehub/src/utils/sanity/client.js
+++ gamehub/src/utils/sanity/client.js
@@ -1,14 +1,10 @@
-import {createClient} from '@sanity/client'
+import {createClient} from '@sanity/client';
 
-export const client = createClient({
-    projectId: "sqhrw5zf",
-    dataset: "my-games",
-    useCdn: true,
-    apiVersion: "2021-10-21"
-})
+const client = createClient({
+  projectId: 'sqhrw5zf',
+  dataset: 'production',
+  useCdn: true, 
+  apiVersion: "2021-10-21"
+});
 
-export const writeClient = createClient({
-    token: "skEsidGaeHVqzFH66RdZMk257gjVKLwATD25i984iL2JdsAkGHFAp7jg8yUIRM1BRK0yEjCjubSBGc5FHvqVqxMah0mORXeEpuqIETwRxWSQzdaB1PyhiPHxcyqwxGHisWBlRRrthu5WwCT9Ve7vd6XMV9jLuBZVC4KYQaUZuBUzFzck7r4t",
-    projectId: "sqhrw5zf",
-    dataset: "my-games"
-})
+export default client;
