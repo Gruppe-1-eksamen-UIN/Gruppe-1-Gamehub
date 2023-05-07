a0444a1f2fb38fb5c9e3f8980ab95f8c815a3ba7
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-05-07 22:31:49 +0200

    Url henting av bilder
    Fant ut hvordan vi skulle hente URL fra et bilde som ligger i Sanity son at den kommer ikke fra Undefined.

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import './css/main.css'
 import client from './utils/sanity/client';
+import imageUrlBuilder from '@sanity/image-url'
 
 import {useState, useEffect} from "react"
 
 function App() {
   const [products, setProducts] = useState([]);
+  const builder = imageUrlBuilder(client);
+  
+  function urlFor(source) {
+    return builder.image(source)
+  }
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,7 +27,7 @@ function App() {
       <ul>
         {products.map((product) => (
           <li key={product._id}>
-            <img src={product.image.asset.url + '?w=200&h=200&fit=crop'} alt={product.name} />
+            <img src={urlFor(product.image) + '?w=200&h=200&fit=crop'} alt={product.name} />
             <h2>{product.name}</h2>
             <p>{product.genre}</p>
             {console.log(product.image.asset.url)}
