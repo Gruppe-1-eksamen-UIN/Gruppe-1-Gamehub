a7ee9f45ec94fbfe7a5d57611191593abbe16e0f
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-05-09 16:53:30 +0200

    Satt opp ny branch
    Satt opp sammen API endringer med Setup endringer.

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 import './css/main.css'
-import client from './utils/sanity/client';
-import imageUrlBuilder from '@sanity/image-url'
-import {useState, useEffect} from "react"
 import axios from 'axios'
 import Dashboard from './components/Dashboard';
 import './css/main.css'
+import { useState, useEffect } from 'react';
+
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const builder = imageUrlBuilder(client);
-  
-  function urlFor(source) {
-    return builder.image(source)
-  }
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const result = await client.fetch('*[_type == "product"]{name, genre, image}');
-      setProducts(result);
-    };
-    fetchProducts();
-  }, []);
-
   const [game, setGame] = useState([]);
 
   const find = () =>{
@@ -39,29 +23,12 @@ function App() {
   }, [])
 
   return (
-
-  
-   
    <div className='nav'>
     <h1>Group 1-Gamehub</h1>
     <Dashboard/>
     
    </div>
    
-
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product._id}>
-            <img src={urlFor(product.image) + '?w=200&h=200&fit=crop'} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>{product.genre}</p>
-            {console.log(product.image.asset.url)}
-          </li>
-        ))}
-      </ul>
-    </div>
   );
 }
 
--- gamehub/src/components/Dashboard.js
+++ gamehub/src/components/Dashboard.js
@@ -1,14 +1,22 @@
 import Favourites from "./Favourites";
 import GameShop from "./GameShop";
 import MyGames from "./MyGames";
+import SanityGames from "./SanityGames"
 
 // Component for Dashboard som viser forsiden med mygames, favourites og gameshop
 export default function Dashboard(){
     return(
         <>
+        <nav>
        <button className="button"><MyGames/></button> 
        <button className="button"><Favourites/></button> 
        <button className="button"><GameShop/></button> 
+        </nav>
+        <SanityGames/>
+   
         </>
+        
+        
+
     )
 }
