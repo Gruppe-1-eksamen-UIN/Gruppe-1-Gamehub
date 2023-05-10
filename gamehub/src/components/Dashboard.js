d1e24a77dab06bcca5b1423e6a49c68290746322
Jadeigo <56861971+Jadeigo@users.noreply.github.com>
2023-05-10 20:37:51 +0200

    FLyttet over noen ting|

--- gamehub/src/components/Dashboard.js
+++ gamehub/src/components/Dashboard.js
@@ -2,17 +2,34 @@ import Favourites from "./Favourites";
 import GameCard from "./GameCard";
 import GameShop from "./GameShop";
 import MyGames from "./MyGames";
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // Component for Dashboard som viser forsiden med mygames, favourites og gameshop
 export default function Dashboard(){
-    return(
+    const [games, setGames] = useState([]);
+    
+    useEffect(() => {
+        axios.get('https://api.rawg.io/api/games?genres=adventure&page_size=20&key=5d8741db23a14d7f88a2c6ccd843ee6c')
+          .then((response) => {
+            setGames(response.data.results);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
+
+    return (
         <>
         <div className="nav">
         <h1>Gamehub</h1>
        <button className="button"><MyGames/></button> 
        <button className="button"><Favourites/></button> 
        <button className="button"><GameShop/></button> 
+        </div>
+
+        <div className="my-games">
+            <GameCard games={games} />
         </div>
        
         </>
@@ -22,3 +39,4 @@ export default function Dashboard(){
 
     )
 }
+
