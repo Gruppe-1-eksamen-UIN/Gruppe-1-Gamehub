fc098f023e9e1f35dea5ba93c83bdce36c7b8da0
Muhsin310302 <Muhsin310302@hotmail.com>
2023-05-12 12:14:45 +0200

    route
    route

--- gamehub/src/App.js
+++ gamehub/src/App.js
@@ -7,48 +7,24 @@ import MyGames from './components/MyGames';
 import Favourites from './components/Favourites';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 
 function App() {
   const [game, setGame] = useState([]);
   const [newGames, setNewGames] = useState([]);
   const [myGames, setMyGames] = useState([]);
 
-  const find = () => {
-    axios
-      .get('https://api.rawg.io/api/games?key=5d8741db23a14d7f88a2c6ccd843ee6c&ordering=-added')
-      .then((response) => {
-        console.log(response);
-        setGame(response.data.results.slice(0, 10));
-      });
-  };
 
-    // useEffect(() => {
-    //   axios
-    //     .get(
-    //       "https://api.rawg.io/api/games?key=5d8741db23a14d7f88a2c6ccd843ee6c&ordering=-released"
-    //     )
-    //     .then((response) => {
-    //       setNewGames(response.data.results.slice(0, 10));
-    //     });
-    // }, []);
-  
-
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "https://api.rawg.io/api/games?key=5d8741db23a14d7f88a2c6ccd843ee6c&genres=action"
-  //     )
-  //     .then((response) => {
-  //       setMyGames(response.data.results.slice(0, 10));
-  //     });
-  // }, []);
 
   return (
     
     <Router>
       <div >
-      <h1 className="Title">Not-Steam</h1>
+        <Link to="/">
+        <h1 className="Title">Not-Steam</h1>
+        </Link>
+      
         <Navbar/>
         <Routes>
           
