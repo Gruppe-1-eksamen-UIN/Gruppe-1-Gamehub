6d9472933962d26ae36bed5aa80cda2875300046
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2023-05-19 00:18:57 +0200

    Update Client.js

--- gamehub/src/utils/sanity/Client.js
+++ gamehub/src/utils/sanity/Client.js
@@ -1,2 +1,10 @@
+import { createClient } from '@sanity/client';
 
-export default function Client();
+const client = createClient({
+  projectId: 'sqhrw5zf',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2021-10-21'
+});
+
+export default client;
