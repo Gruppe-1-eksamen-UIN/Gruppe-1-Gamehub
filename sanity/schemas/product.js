cbe373a72d873dcf6ec2dc84639cac735ac937c2
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2023-05-18 15:20:37 +0200

    sanity schema updatw

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -1,26 +1,46 @@
 export default {
-    name: 'product',
-    title: 'Product',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 200,
       },
-      {
-        name: 'genre',
-        title: 'Genre',
-        type: 'string',
+    },
+    {
+      name: 'apiId',
+      title: 'API ID',
+      type: 'string',
+    },
+    {
+      name: 'playedTime',
+      title: 'Game Played Time',
+      type: 'number',
+    },
+    {
+      name: 'genre',
+      title: 'Genre',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true,
-        },
-      },
-    ],
-  };
+    },
+  ],
+};
+
   
